61108b2769f6f3be122e5133e7caa0d637cc844f
Ganesh Ghadage <ganeshvg22@gmail.com>
2024-08-28 10:51:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# News Letter : Your Daily Need
+
+This is news web app. It helps you to keep updated with news around you.
---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo() {
+    return (
+        <p>Logo</p>
+    )
+}
+
+export default Logo
