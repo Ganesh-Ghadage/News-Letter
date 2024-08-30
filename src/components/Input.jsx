b75d3ce542c11571878365e92cd03a463509cc08
Ganesh Ghadage <ganeshvg22@gmail.com>
2024-08-30 19:00:35 +0530

    Added the props attribute for input to have more attributes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Input({value, onChange}) {
+function Input({value, onChange, props}) {
     return (
         <div className="flex flex-wrap items-center justify-center sm:col-span-3">
             <label htmlFor="Search" className="hidden md:flex text-lg font-medium leading-6 text-white pr-2">
@@ -13,6 +13,7 @@ function Input({value, onChange}) {
                 type="text"
                 value={value}
                 onChange={onChange} 
+                {...props}
                 placeholder='Search your intrest...'
                 className="inline w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
