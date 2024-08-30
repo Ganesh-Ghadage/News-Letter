25f5033d71ff58e61b64b701423aa7d276532f53
Ganesh Ghadage <ganeshvg22@gmail.com>
2024-08-30 19:02:02 +0530

    removed unnecessary import statement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Error, Headlines, Input, Loader, MyCard } from '../components'
 import { reactCustomQuery } from '../reactCustomQuery/reactCustomQuery'
